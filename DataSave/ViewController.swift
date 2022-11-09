e3f063d83af75584b0d59e6c7335985e5f53abba
burak <burak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.com>
2022-11-09 20:42:11 +0300

    data save

--- DataSave/ViewController.swift
+++ DataSave/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var surnameText: UITextField!
+    @IBOutlet weak var hereText: UITextView!
+    @IBOutlet weak var surnameHere: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storeName = UserDefaults.standard.object(forKey: "name")
+        let storeSurname = UserDefaults.standard.object(forKey: "surname")
+        
+        if let myName = storeName as? String{
+            hereText.text = "Name: \(myName)"
+        }
+        
+        
+        if let mySurname = storeSurname as? String{
+            surnameHere.text = "Surname: \(mySurname)"
+        }
+        
+        
     }
 
-
+    @IBAction func saveclick(_ sender: Any) {
+    
+        UserDefaults.standard.set(nameText.text!, forKey: "name")
+        UserDefaults.standard.set(surnameText.text!, forKey: "surname")
+        
+        hereText.text = "Name: \(nameText.text!)"
+        surnameHere.text = "Surname: \(surnameText.text!)"
+        
+    }
+    
 }
 
